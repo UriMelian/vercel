a73815fb8880965798592b2a692c0997dd95aedf
UriMelian <Urimelian28@gmail.com>
2026-02-27 15:24:39 -0300

    Update database configuration parameters

--- API de gestion de datos/config.py
+++ API de gestion de datos/config.py
@@ -1,11 +1,9 @@
 DB_HOST = (
-"aws-0-us-west-2.pooler.supabase.com"  #  Aca va el host de tu 
-base de datos
+"aws-0-us-west-2.pooler.supabase.com" 
 )
-DB_PORT  "6543"  #  Este es siempre el mismo
-DB_NAME  "postgres"  #  Aca va el nombre de tu base de datos
-DB_USER  "postgres.emppogsoxoykmsxxmpyj"  #  Aca va el usuar
-io de tu base de datos
-DB_PASSWORD  "QQRg4mA3VvqgU9bP"  #  Aca va la contraseña 
-de tu base de datos
-DB_SSLMODE  "require"  #  Este siempre es lo mismo
+DB_PORT  "6543" 
+DB_NAME  "APIgd" 
+DB_USER  "urimelian28@gmail.com" 
+DB_PASSWORD  "RoMa201221"  
+DB_SSLMODE  "require" 
+
