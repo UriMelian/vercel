7824e24f78334610bcb4540d351e40915992aff9
UriMelian <Urimelian28@gmail.com>
2026-02-27 15:29:42 -0300

    Fix formatting issues in config.py

--- API de gestion de datos/config.py
+++ API de gestion de datos/config.py
@@ -1,9 +1,10 @@
 DB_HOST = (
 "aws-0-us-west-2.pooler.supabase.com" 
 )
-DB_PORT  "6543" 
-DB_NAME  "APIgd" 
-DB_USER  "urimelian28@gmail.com" 
-DB_PASSWORD  "RoMa201221"  
-DB_SSLMODE  "require" 
+DB_PORT = "6543" 
+DB_NAME = "APIgd" 
+DB_USER = "urimelian28@gmail.com" 
+DB_PASSWORD = "RoMa201221"  
+DB_SSLMODE = "require" 
+
 
